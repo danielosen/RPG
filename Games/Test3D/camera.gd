a40cd747555238b5ab4f8a80ed70809a996165c8
danielosen <danieleo@student.matnat.uio.no>
2016-12-14 17:04:53 +0100

    Add files via upload

--- Games/Test3D/camera.gd
+++ Games/Test3D/camera.gd
@@ -1,46 +1,67 @@
 extends Camera
-
+# gamepad states
+export var bool_controller_playstation = true
+export var bool_controller_keyboard = false
+# joystick test
+var  joystick_input_sensitivity = 0.1
+var joystick_motion = Vector2()
+var is_zooming = false
 # Distance from the target
-var radius = 10.0
-var max_radius = 60.0
+var radius = 40.0
+export var max_radius = 60.0
 var min_radius = 5.0
 # Here we assume the target will be the parent, but could be any node
 onready var target = get_parent()
-onready var mesh = get_parent().get_node("PlayerMesh")
+onready var ray = get_node("CameraRay")
 # Horizontal angle
 var _yaw = 0
 # Vertical angle
-var _pitch = 0
+var _pitch = 0.4
 
 # The vertical angle must be limited otherwise we would be able to look upside down
-var _pitch_min = -PI/2+0.4
-var _pitch_max = PI/2-0.4
+var _pitch_min = -PI/2+0.5
+var _pitch_max = PI/2-0.5
 
 # How fast the camera moves when the mouse moves (in degrees per pixel)
 var sensitivity = Vector2(1,1)
+var joystick_sensitivity = Vector2(3,2)
 
 var pressed = false
 var last_mouse_pos = Vector2()
 
 
 func _ready():
-    set_process_input(true)
-    set_fixed_process(true)
+	set_process_input(true)
+	set_fixed_process(true)
+	ray.add_exception(target)
 
 
 # Technically not needed in the current scene,
 # but would be needed if the camera was not a child of the avatar
 func _fixed_process(delta):
+	ray.set_cast_to(target.get_translation())
 	_update_transform()
 
-
 func _update_transform():
+	if bool_controller_playstation:
+		_yaw += joystick_motion.x*deg2rad(joystick_sensitivity.x)
+		if is_zooming:
+			radius -= joystick_motion.y
+			radius = clamp(radius,min_radius,max_radius)
+		else:
+			_pitch += joystick_motion.y *deg2rad(joystick_sensitivity.y)
+			_pitch = clamp(_pitch, _pitch_min, _pitch_max)
 	var target_pos = target.get_translation()
 	# 3D trigonometry: calculate position from X and Y angles.
 	# The 3rd one (Z, or roll) would not affect the position so we ignore it
 	var pos = Vector3(cos(_yaw) * cos(_pitch), sin(_pitch), sin(_yaw) * cos(_pitch))
 	# Adjust distance from target
+	
+	if ray.is_colliding():
+		#radius *= 0.99
+		pass
 	pos *= radius
+	# Need Collision Detection!
 	# Set the actual position and rotation.
 	# (note: if a 3rd angle Z is needed, it would affect the up vector, last param)
 	look_at_from_pos(target_pos + pos, target_pos, Vector3(0,1,0))
@@ -48,22 +69,41 @@ func _update_transform():
 
 func _input(event):
 	# Did the mouse move?
-	if event.type == InputEvent.MOUSE_BUTTON:
-		if event.button_mask == 1:
-			pressed = true
-		elif event.button_mask == 0:
-			pressed = false
-		if event.button_index == 5:
-			if radius < max_radius:
-				radius += 1.0
-		elif event.button_index == 4:
-			if radius > min_radius:
-				radius -= 1.0
-	elif event.type == InputEvent.MOUSE_MOTION and pressed:
-		# Get how many pixels it moved
-		var rmotion = event.relative_pos
-		# Apply this to rotations
-		_yaw += rmotion.x * deg2rad(sensitivity.x)
-		_pitch += rmotion.y * deg2rad(sensitivity.y)
-		_pitch = clamp(_pitch, _pitch_min, _pitch_max)
-		_update_transform()
+	if bool_controller_keyboard:
+		if event.type == InputEvent.MOUSE_BUTTON:
+			if event.button_mask == 1:
+				pressed = true
+			elif event.button_mask == 0:
+				pressed = false
+			if event.button_index == 5:
+				if radius < max_radius:
+					radius += 1.0
+			elif event.button_index == 4:
+				if radius > min_radius:
+					radius -= 1.0
+		elif event.type == InputEvent.MOUSE_MOTION and pressed:
+			# Get how many pixels it moved
+			var rmotion = event.relative_pos
+			# Apply this to rotations
+			_yaw += rmotion.x * deg2rad(sensitivity.x)
+			_pitch += rmotion.y * deg2rad(sensitivity.y)
+			_pitch = clamp(_pitch, _pitch_min, _pitch_max)
+			_update_transform()
+	elif bool_controller_playstation:
+		if event.type == InputEvent.JOYSTICK_BUTTON:
+			if event.button_index == 8:
+				is_zooming = event.is_pressed()
+		if event.type == InputEvent.JOYSTICK_MOTION:
+			#the right joystick only reports when there is relative movement!
+			#default is inverted, changed to push left camera walks left etc.
+			if event.axis == 2: #-1: left, +1: right
+				if abs(event.value) > joystick_input_sensitivity:
+					joystick_motion.x = -event.value
+				else:
+					joystick_motion.x = 0
+			if event.axis == 3: #+1: up, -1: down
+				if abs(event.value) > joystick_input_sensitivity:
+					joystick_motion.y = -event.value
+				else:
+					joystick_motion.y = 0
+		
--- Games/Test3D/player2.gd
+++ Games/Test3D/player2.gd
@@ -1,76 +1,150 @@
 extends KinematicBody
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+### Gamepad States ###
+var bool_controller_playstation = true
+var bool_controller_keyboard = false
+var joystick_motion = Vector2()
+export var  joystick_input_sensitivity = 0.1
+### End Gamepad States ###
+
+### Movement States ###
 var bool_is_moving_forward = false
 var bool_is_moving_backward = false
+var bool_is_strafing_left = false
+var bool_is_strafing_right = false
+### End Movement States ###
+
+### Collision States ###
+var bool_is_on_ground = false
+var bool_is_on_slope = false
+var bool_is_falling = false
+### End Collision States ###
+
+### Gravity and Jump States ###
+var bool_is_jumping = false
+var jump_time = 0.0
+var jump_force = 2.0
+var grav_time = 0.0
+var grav_force = 1.7
+### End Gravityy States ###
+
+### Node Loading ###
 onready var mesh = get_node("PlayerMesh")
 onready var camera = get_node("PlayerCamera")
+onready var ray_down = get_node("PlayerRayDown")
+### End Node Loading ###
+
+### Init Node ###
 func _ready():
 	set_process_input(true)
 	set_fixed_process(true)
-	#init camera_facing
 	pass
+### End Init Node ###
 
+### Parse Input ###
 func _input(event):
-	if event.type == InputEvent.KEY:
-		if event.scancode == KEY_W:
-			bool_is_moving_forward = event.is_pressed()
-		elif event.scancode == KEY_S:
-			bool_is_moving_backward = event.is_pressed()
+	if bool_controller_keyboard:
+		if event.type == InputEvent.KEY:
+			if event.scancode == KEY_W:
+				bool_is_moving_forward = event.is_pressed()
+			elif event.scancode == KEY_S:
+				bool_is_moving_backward = event.is_pressed()
+			elif event.scancode == KEY_A:
+				bool_is_strafing_left = event.is_pressed()
+			elif event.scancode == KEY_D:
+				bool_is_strafing_right = event.is_pressed()
+			elif event.scancode == KEY_SPACE:
+				if bool_is_jumping == false and bool_is_on_ground == true:
+					bool_is_jumping = true
+	elif bool_controller_playstation:
+		if event.type == InputEvent.JOYSTICK_MOTION:
+			#parses two events at the same time
+			#axis 1 up -1 to down +1, axis 0 left -1 to right +1
+			if event.axis == 0:
+				if abs(event.value) >= joystick_input_sensitivity:
+					joystick_motion.x = event.value
+				else:
+					joystick_motion.x = 0
+			if event.axis == 1:
+				if abs(event.value) >= joystick_input_sensitivity:
+					joystick_motion.y = event.value
+				else:
+					joystick_motion.y = 0
+		if event.type == InputEvent.JOYSTICK_BUTTON:
+			if event.button_index == 0: #sony x button
+				if bool_is_jumping == false and bool_is_on_ground == true:
+					bool_is_jumping = true
+				
 	pass
+### End Parse Input ###
+
+### Process Input ###
 func _fixed_process(delta):
-	
+	#Get transformation states
+	#camera basis is not orthonormal, only orthogonal
 	var cameraForward = -camera.get_transform().basis.z
 	cameraForward.y = 0
-	var playerForward = -mesh.get_transform().basis.z
-	playerForward.y = 0
-	var angle = cameraForward.angle_to(playerForward)
-	if is_nan(angle):
-		angle = 0
-	var z_diff = cameraForward.z-playerForward.z
-	if is_nan(z_diff):
-		z_diff = 0
-	var motion = cameraForward
-	angle = atan2(motion.x,-motion.z)
+	var cameraLeft = -camera.get_transform().basis.x
+	var meshForward = -mesh.get_transform().basis.z
+	var meshLeft = -mesh.get_transform().basis.x
+	meshLeft.y = 0
+	meshForward.y = 0
+	var angle = atan2(cameraForward.x,-cameraForward.z)
 	
-	#print("angle: " +str(angle)+ "z_diff: " + str(z_diff))
-	#mesh.rotate_y(sign(-z_diff)*angle)
-	"""
-	if is_nan(angle):
-		angle = 0
-	var z_diff = cameraForward.z-playerForward.z
-	print("angle: " +str(angle)+ "z_diff: " + str(z_diff))
-	while(angle*180/PI > 0.01):
-		mesh.rotate_y(sign(-z_diff)*angle)
-		pass
-	"""
-	#if abs(playerForward.angle_to(cameraForward)) > 1:
-	#	look_at(get_translation()-cameraForward,Vector3(0,1,0));
-	### next task, make player rotate in camera forward viewing direction
-	
-	if bool_is_moving_forward:
-		Rotation(angle)
-		move(playerForward)
-	"""
-	elif bool_is_moving_backward:
-		var dir = facing.basis.z
-		dir.y = 0
-		move(get_transform().basis.x)
-		#move(dir.basis.x)
-		#look_at(dir+get_translation(),Vector3(0,1.0,0))
-		#move(dir.normalized())
+	#Process Movement
+	if bool_controller_keyboard:
+		if bool_is_moving_forward:
+			rotate_mesh(angle)
+			move(meshForward)
+		elif bool_is_moving_backward:
+			#rotate_mesh_to_camera(PI+angle)
+			move(-meshForward)
+		elif bool_is_strafing_left:
+			#rotate_mesh(angle-PI/2)
+			#move(meshForward)
+			move(meshLeft)
+		elif bool_is_strafing_right:
+			#rotate_mesh(angle+PI/2)
+			#move(meshForward)
+			move(-meshLeft)
+	elif bool_controller_playstation:
+		#the ps controller isnt normalized for a given pressure
+		#it seems to favor up and left on the lstick
+		var joystick_speed = min(joystick_motion.length(),1.0)
+		#camera basis vectors are not orthonormal..
+		var mesh_motion = -joystick_motion.y*cameraForward-joystick_motion.x*cameraLeft
+		var testangle = atan2(mesh_motion.x,-mesh_motion.z)
+		if joystick_speed > 0:
+			rotate_mesh(testangle)
+			move(mesh_motion.normalized()*joystick_speed)
 		
-		#look_at(dir-get_translation(),Vector3(0,1.0,0))
-		#move(get_rotation()))
-		#rotate_y(dir.z*delta)
-		"""
-	pass
+	#Process Collision
+	grav_time += delta
+	if ray_down.is_colliding():
+		var n = ray_down.get_collision_normal()
+		if n.y == 1.0 and n.x == 0 and n.z == 0:
+			bool_is_on_ground = true
+			grav_time = 0.0
+	else:
+		bool_is_on_ground = false
 	
-func Rotation(var angle):
-    var delta = get_fixed_process_delta_time()
-    var current_rot = Quat(mesh.get_transform().basis)
-    var target_rot = Quat(Vector3(0,1,0), angle)
-    var smooth_rot = current_rot.slerp(target_rot, delta * 10)
-    mesh.set_rotation(Matrix3(smooth_rot).get_euler())
+	#Process Gravity
+	if bool_is_jumping == true:
+		jump_time += delta
+		move(Vector3(0,jump_force*pow(1-jump_time,2),0.0))
+		bool_is_jumping = jump_time < 1.0
+	else:
+		jump_time = 0.0
+	if bool_is_on_ground == false:
+		move(Vector3(0,-grav_force*pow(grav_time,2),0))
+### End Process Input ###
+
+
+### Movement Functions ###
+func rotate_mesh(var angle):
+	var delta = get_fixed_process_delta_time()
+	var current_rot = Quat(mesh.get_transform().basis)
+	var target_rot = Quat(Vector3(0,1,0), angle)
+	var smooth_rot = current_rot.slerp(target_rot, delta * 10)
+	mesh.set_rotation(Matrix3(smooth_rot).get_euler())
+### End Movement Functions ###
